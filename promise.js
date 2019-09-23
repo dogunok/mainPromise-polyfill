67436fd8266d2a7bee9078172121d70b995aef82
dogunok <spam-system.s@yandex.ru>
2019-09-23 12:37:49 +0300

    добавил код для полифила

--- promise.js
+++ promise.js
@@ -1,22 +1,43 @@
 (function(){
     function MainPromise(callback){
 
-        function resolve(){
-            
+        if(typeof callback !== 'function'){
+            return console.error('Аргументом была передана не функция')
         }
 
-        function reject(){
+        this.runningProcess = false;
+        this.runQueue = [];
 
-        }
+        this.resolve = function(data) {
+            if (!this.runningProcess) {
+                this.runningProcess = true;
+                this.thenChain.forEach(function(item) {
+                    data = item(data);
+                })
+            }
+        }.bind(this)
+        this.reject = function(error) { 
+            if (!this.runningProcess) {
+                this.runningProcess = true;
+                this.caught(error);
+            }
+        }.bind(this)
 
         setTimeout(function(){
+            try{
+                callback(this.resolve, this.reject)
+            } catch(error){
+                this.reject(error)
+            }
 
+        }.bind(this), 0)
 
-
-        }, 0)
-
+        this.then = function(funcThen){
+            this.runQueue.push(funcThen)
+        }.bind(this)
 
     }
-
-    window.MainPromise = MainPromise;
+    if(window.Promise === undefined){
+        window.MainPromise = MainPromise;
+    }
 })()
